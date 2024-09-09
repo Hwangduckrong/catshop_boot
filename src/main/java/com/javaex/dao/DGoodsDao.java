ce4964c60d1c43c874a585288822db9cf26d0548
황덕룡 <hwangsee12@naver.com>
2024-09-09 15:52:08 +0900

    메인화면 상품정보등록 성공

---
+++ src/main/java/com/javaex/dao/DGoodsDao.java
@@ -0,0 +1,29 @@
+package com.javaex.dao;
+
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.javaex.vo.GoodsVo;
+
+@Repository
+public class DGoodsDao {
+	
+	@Autowired
+	private SqlSession sqlSession;
+	
+	public List<GoodsVo> getmainList(){
+		
+		List<GoodsVo> goodsList = sqlSession.selectList("goods.selectGoods");
+
+		
+		return goodsList;
+	}
+	
+
+	
+}
+	
+
